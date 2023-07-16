614f743ead6c4e4368021c1e267eacb374ef820a
Dave Huang <dave@hooked.team>
2023-07-16 23:47:09 +0800

    Update echo bot

--- echo-bot/bot.js
+++ echo-bot/bot.js
@@ -8,7 +8,13 @@ Hi human:
 
 /start - start the bot
 /help - command reference
+/echo - echo what you said
 `;
+
+bot.use((ctx, next) => {
+  console.log(`${ctx.from.username}: ${ctx.message.text}`);
+  next();
+});
 
 bot.start(ctx => {
   ctx.reply(`Hi, I'm echo bot!`);
@@ -17,6 +23,22 @@ bot.start(ctx => {
 
 bot.help(ctx => {
   ctx.reply(HELP_MSG);
+});
+
+bot.command('echo', ctx => {
+  let input = ctx.message.text;
+  let inputArr = input.split(' ');
+
+  let message = '';
+
+  if (inputArr.length == 1) {
+    message = 'You said echo';
+  } else {
+    inputArr.shift();
+    message = inputArr.join(' ');
+  }
+
+  ctx.reply(message);
 });
 
 bot.launch();
