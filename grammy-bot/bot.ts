13cc1c1280f0e5316c1727e9f81cead1bc3b7a89
Dave H <bugman195@gmail.com>
2023-07-24 22:00:46 +0800

    Add middleware with context flavor

--- grammy-bot/bot.ts
+++ grammy-bot/bot.ts
@@ -1,16 +1,46 @@
-import { Bot } from 'grammy';
+import { Bot, Context } from 'grammy';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '../.env' });
 
 const bot = new Bot(process.env.TUTOR_BOT_KEY || '');
 
+interface ContextFlavor<S> {
+  config: {
+    isAdmin: S;
+  };
+}
+
+type MyContext = Context & ContextFlavor<boolean>;
+
+/**
+ * Custom middleware
+ */
+bot.use(async (ctx, next) => {
+  (ctx as MyContext).config = {
+    isAdmin: `${ctx.from?.id}` === process.env.UID,
+  };
+  await next();
+});
+
 bot.command('start', async ctx => {
   // prettier-ignore
-  ctx.reply('Welcome\\! \*Up\* Up \_\_and\_\_ \_running\_\\.', {
+  await ctx.reply('Welcome\\! *Up* __and__ _running_\\.', {
     parse_mode: 'MarkdownV2',
+    reply_markup: {force_reply: true}
   });
 });
-bot.on('message', ctx => ctx.reply('Got another message!'));
+bot.on('message', async ctx => {
+  const withStart = await ctx.hasCommand('start');
+  console.log('With command start?', withStart);
+
+  const { isAdmin } = await (ctx as any).config;
+  const text = 'Got another message!';
+
+  // prettier-ignore
+  await ctx.reply(isAdmin ? `Oh mighty <b style="color:red;">Admin</b>! ${text}` : text, {
+    parse_mode: 'HTML',
+  });
+});
 
 bot.start();
