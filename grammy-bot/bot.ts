b04426980f3524b1e71a17e6e3200a4c3bda6b5e
Dave H <bugman195@gmail.com>
2023-07-24 22:18:42 +0800

    Use filter pattern

--- grammy-bot/bot.ts
+++ grammy-bot/bot.ts
@@ -30,17 +30,27 @@ bot.command('start', async ctx => {
     reply_markup: {force_reply: true}
   });
 });
+
+bot.on('message:text').filter(
+  async ctx => {
+    const { isAdmin } = await (ctx as any).config;
+    return isAdmin;
+  },
+  async ctx => {
+    await ctx.reply(
+      'Oh mighty <b style="color:red;">Admin</b> sent a message!',
+      {
+        parse_mode: 'HTML',
+      }
+    );
+  }
+);
+
 bot.on('message:text', async ctx => {
   const withStart = await ctx.hasCommand('start');
   console.log('With command start?', withStart);
 
-  const { isAdmin } = await (ctx as any).config;
-  const text = 'Got another message!';
-
-  // prettier-ignore
-  await ctx.reply(isAdmin ? `Oh mighty <b style="color:red;">Admin</b>! ${text}` : text, {
-    parse_mode: 'HTML',
-  });
+  await ctx.reply('Just a message');
 });
 
 bot.on('message:photo', ctx => {
