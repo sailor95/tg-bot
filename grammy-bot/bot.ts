b160563982c72063e54d34453da6d9501452ff32
Dave H <bugman195@gmail.com>
2023-07-25 21:18:41 +0800

    Add errorBoundary & composer

--- grammy-bot/bot.ts
+++ grammy-bot/bot.ts
@@ -1,4 +1,12 @@
-import { Bot, Context, GrammyError, HttpError } from 'grammy';
+import {
+  Bot,
+  Context,
+  GrammyError,
+  HttpError,
+  Composer,
+  BotError,
+  NextFunction,
+} from 'grammy';
 import dotenv from 'dotenv';
 
 dotenv.config({ path: '../.env' });
@@ -17,11 +25,54 @@ type MyContext = Context & ContextFlavor<boolean>;
  * Custom middleware
  */
 bot.use(async (ctx, next) => {
+  console.log('Middleware X');
   (ctx as MyContext).config = {
     isAdmin: `${ctx.from?.id}` === process.env.UID,
   };
   await next();
 });
+
+const composer = new Composer();
+composer.use(async (ctx, next) => {
+  console.log('Middleware A');
+  await next();
+});
+composer.use(async (ctx, next) => {
+  console.log('Middleware B');
+  await next();
+});
+bot.errorBoundary(boundaryHandler /** Q */).use(composer);
+
+/**
+ * Error in Q, A, or B
+ */
+async function boundaryHandler(err: BotError, next: NextFunction) {
+  // Run middleware at C in case of error
+  await next();
+}
+
+bot.use(async (ctx, next) => {
+  console.log('Middleware C');
+  await next();
+});
+
+bot.catch(errorHandler);
+
+/**
+ * Error in X or C
+ */
+function errorHandler(err: BotError) {
+  const ctx = err.ctx;
+  console.error(`Error while handling update ${ctx.update.update_id}:`);
+  const e = err.error;
+  if (e instanceof GrammyError) {
+    console.error('Error in request:', e.description);
+  } else if (e instanceof HttpError) {
+    console.error('Could not contact Telegram:', e);
+  } else {
+    console.error('Unknown error:', e);
+  }
+}
 
 bot.api.setMyCommands([
   { command: 'start', description: 'Start the bot' },
@@ -90,18 +141,4 @@ bot.on('message:photo', ctx => {
   ctx.reply('Good photo!');
 });
 
-bot.catch((err) => {
-  const ctx = err.ctx;
-  console.error(`Error while handling update ${ctx.update.update_id}:`);
-  const e = err.error;
-  if (e instanceof GrammyError) {
-    console.error("Error in request:", e.description);
-  } else if (e instanceof HttpError) {
-    console.error("Could not contact Telegram:", e);
-  } else {
-    console.error("Unknown error:", e);
-  }
-});
-
 bot.start();
-
